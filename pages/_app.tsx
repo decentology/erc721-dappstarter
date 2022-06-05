b16a137973ee883d4cf0be455d10295a35573a16
Angelica Turla <turla_angelica@yahoo.com>
2022-06-05 18:01:29 -0400

    hyperverse stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { initialize, Provider, Network } from '@decentology/hyperverse'
+import { Ethereum } from '@decentology/hyperverse-ethereum'
+import * as NFT from '@decentology/hyperverse-evm-erc721'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const hyperverse = initialize({
+    blockchain: Ethereum,
+    network: Network.Testnet,
+    modules: [
+      {
+        bundle: NFT,
+        // add the account address you used to initalize your instance
+        tenantId: '',
+      },
+    ],
+  })
+  return (
+    <Provider initialState={hyperverse}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
